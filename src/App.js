4bbd78214070d1f7edb23d8d522ecddaea52d5b3
TheAswin <75930721+TheAswin@users.noreply.github.com>
2021-04-09 11:13:19 +0530

    ENV test Commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import Home from "./screens/Home";
 import NewNote from "./screens/NewNote";
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import abcd from "./envTest";
 
 function App() {
+  //
+  useEffect(() => {
+    abcd();
+    console.log(process.env.REACT_APP_NOT_SECRET_CODE);
+  }, []);
+
   return (
     <div className="app">
       <Router>
---
+++ src/envTest.js
@@ -0,0 +1,5 @@
+console.log(process.env.REACT_APP_NOT_SECRET_CODE);
+
+export default function abcd() {
+  console.log(process.env.REACT_APP_NOT_SECRET_CODE);
+}
